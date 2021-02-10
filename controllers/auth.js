536853fcee9a2f0ea7ac5d79603b24b026b73fc8
matthewrapp <mattrapp25@gmail.com>
2021-02-10 11:40:27 -0700

    another try

--- controllers/auth.js
+++ controllers/auth.js
@@ -230,7 +230,7 @@ exports.postReset = (req, res, next) => {
                     subject: 'Password Reset',
                     html: `
                         <p>You requested password reset.</p>
-                        <p>Click this <a href="/${token}">link</a> to set a new password.</p>
+                        <p>Click this <a href="/reset/${token}">link</a> to set a new password.</p>
                     `
                 });
             })
