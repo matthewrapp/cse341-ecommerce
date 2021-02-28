26e4dcefb964ecb7dfb793c134b56fbe4ce82461
matthewrapp <mattrapp25@gmail.com>
2021-02-28 11:14:20 -0700

    Adding Pagination to the Home Page

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,9 @@ const Order = require('../models/order');
 const getCircularReplacer = require('../utilities/circular-replacer');
 const product = require('../models/product');
 
+// global variables
+const ITEMS_PER_PAGE = 3;
+
 exports.getProducts = (req, res, next) => {
     Product.find()
         .then(products => {
@@ -76,13 +79,29 @@ exports.getSearchedProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
+    const page = parseInt(req.query.page) || 1;
+    let totalItems;
+    
     Product.find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
             res.render('shop/index', {
                 docTitle: 'Shop Page',
                 path: '/',
                 prods: products,
-                searchProduct: null
+                searchProduct: null,
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
